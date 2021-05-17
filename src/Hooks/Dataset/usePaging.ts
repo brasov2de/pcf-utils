3f42aedd0adee11e24fc545106702cef1e42ed46
Diana <diana.birkelbach@orbis.de>
2021-05-17 21:11:59 +0200

    introduced usePaging

---
+++ src/Hooks/Dataset/usePaging.ts
@@ -0,0 +1,55 @@
+import * as React from 'react';
+type DataSet = ComponentFramework.PropertyTypes.DataSet;
+
+
+export const usePaging = (dataset: DataSet) => {    
+      
+    const [firstItemNumber, setFirstItemNumber] = React.useState<number>(0);
+    const [lastItemNumber, setLastItemNumber] = React.useState<number>();
+    const [totalRecords, setTotalRecords] = React.useState<number>();
+    const [currentPage, setCurrentPage] = React.useState<number>(0);
+    const [pageSize, setPageSize] = React.useState<number>(0);    
+               
+
+    React.useEffect(() => {
+        if(!dataset.paging.hasPreviousPage){ //first page
+            setPageSize(dataset.sortedRecordIds.length);
+            setCurrentPage(1);
+            setTotalRecords(dataset.paging.totalResultCount);      
+        }               
+        setFirstItemNumber((currentPage-1) * pageSize + 1);
+        setLastItemNumber((currentPage-1) * pageSize + dataset.sortedRecordIds.length )       
+    }, [dataset]);
+
+  
+
+    function moveToFirst(){        
+        setCurrentPage(1);
+        (dataset.paging as any).loadExactPage(1);
+    }
+
+    function movePrevious(){        
+        const newPage = currentPage-1;
+        setCurrentPage(newPage);
+        (dataset.paging as any).loadExactPage(newPage);        
+       
+    }
+
+    function moveNext(){        
+        const newPage = currentPage+1;
+        setCurrentPage(newPage);
+        (dataset.paging as any).loadExactPage(newPage);                
+    }   
+
+    return {       
+        
+        currentPage,
+        firstItemNumber, 
+        lastItemNumber, 
+        totalRecords, 
+        moveToFirst, 
+        movePrevious,
+        moveNext,       
+
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,3 +4,4 @@ export  * from "./Utils/environmentVariable";
 
 export * from "./Hooks/useEnvironmentVariable";
 export * from "./Hooks/useResourceImage";
+export * from "./Hooks/Dataset/usePaging";
